689af7c4cb242c2c46173b52d616428a91f354ef
SomSingh23 <githubsom@gmail.com>
2023-10-27 19:32:34 +0530

    home page with css added, home page contain nav bar,tata power logo and copyright

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 let express = require("express");
-let path = require("path");
 let app = express();
 let getApi = require("./utils/middleware/getApi");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.listen(process.env.PORT, () => {
-  console.log(`Running on port ${process.env.port}....`);
+  console.log(`Running on port ${process.env.PORT}....`);
 });
 app.get("/", (req, res) => {
   res.render("home");
