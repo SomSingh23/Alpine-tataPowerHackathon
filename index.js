026220e35de1f0c4bcf0830a68b1bbcbe360e94c
SomSingh23 <githubsom@gmail.com>
2023-10-27 18:32:39 +0530

    views folder added and tata power logo added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 let express = require("express");
+let path = require("path");
 let app = express();
 let getApi = require("./utils/middleware/getApi");
+app.set("view engine", "ejs");
 app.listen(process.env.PORT, () => {
   console.log(`Running on port ${process.env.port}....`);
 });
 app.get("/", (req, res) => {
-  res.send("Test");
+  res.render("home");
 });
 app.get("/api/ev", getApi, (req, res) => {
   res.json({
--- utils/middleware/getApi.js
+++ utils/middleware/getApi.js
@@ -2,6 +2,7 @@ let getApi = (req, res, next) => {
   if (
     req.headers["authorization"] &&
     req.headers["authorization"] === process.env.header
+    // this header will be replaced in future
   ) {
     next();
   } else res.status(401).send("Unauthorized,use proper Authorization header");
