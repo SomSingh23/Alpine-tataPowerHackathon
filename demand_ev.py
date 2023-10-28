cf383bf32c97d9c06c8f4ae897edba51d257b204
SomSingh23 <githubsom@gmail.com>
2023-10-28 14:12:45 +0530

    css done for this prototype

--- demand_ev.py
+++ demand_ev.py
@@ -1,38 +1,45 @@
-# DEMAND PREDICTION AND SERVICE GAP IDENTIFICATION
+# DEMAND PREDICTION 
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load your dataset
-data = pd.read_csv('ev.csv')
+data = pd.read_csv('EV.csv')
 
-# Create the count plot
 plt.figure(figsize=(12, 6))
 sns.countplot(data=data, x='State', hue='ChargePreference')
 plt.title('Count Plot of Charge Preference by State')
 plt.xlabel('State')
 plt.ylabel('Count')
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.xticks(rotation=45)
 plt.legend(title='Charge Preference')
-plt.savefig(f'./public/ai_gen_img/dp1')
+plt.savefig(f'./dp1')
 # plt.show()
 
 
 # Service Gap: 
-# Places where eco friendly is less i.e. WB, Karnataka - 2 reasons possible ek toh yaa to hamare service centre kam h yaa logo mein jo eco friendly waali jaankari nhi h toh campaign chalao, aur service centre kam h toh vahan kholna padega company ko
 
-# Demand Prediction - Delhi aur TN mein eco friendly ki demand high h toh aage bhi hogi.
+plt.figure(figsize=(12, 6))
+sns.countplot(data=data, x='State', hue='UserType')
+plt.title('Count Plot of UserID by State')
+plt.xlabel('State')
+plt.ylabel('Count')
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.legend(title='STATE WISE DATA')
+plt.savefig(f'./dp1')
+plt.show()
 
-# Note: Demand high ho sakti h par yeh nhi pta ki vaha pe sufficient charging stations h yaa nhi. Agar nhi h toh lagao fir.
-# DEMAND PREDICTION AND SERVICE GAP IDENTIFICATION
+plt.figure(figsize=(12, 6))
+sns.countplot(data=data, x='DailyDrivingDistance', hue='PreferredChargingTime')
+plt.title('Count Plot of Distance and Chargetime')
+plt.xlabel('State')
+plt.ylabel('Count')
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.legend(title='DistaneToCharge')
+plt.savefig(f'./dp1')
+plt.show()
+# People mostly travel long distance during the afternoon period and less likely during the evening
+# Short distances are mostly covered during morning and less likely during evening
+# And an average distance is travelled mostly in the evening and less likely in the morning
 
-
-
-
-# Service Gap: 
-# Places where eco friendly is less i.e. WB, Karnataka - 2 reasons possible ek toh yaa to hamare service centre kam h yaa logo mein jo eco friendly waali jaankari nhi h toh campaign chalao, aur service centre kam h toh vahan kholna padega company ko
-
-# Demand Prediction - Delhi aur TN mein eco friendly ki demand high h toh aage bhi hogi.
-
-# Note: Demand high ho sakti h par yeh nhi pta ki vaha pe sufficient charging stations h yaa nhi. Agar nhi h toh lagao fir.
+# So, the charging stations near the highways must be most active during the afternoon and in the cities during early morning
